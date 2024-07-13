0f7d49341f33a3791b53c1766d9b9e4fe3552609
Irenita-Lopes <irenita.f.lopes@gmail.com>
2024-07-13 16:46:14 -0300

    Adicionando testes conforme desafio

--- queue.js
+++ queue.js
@@ -3,8 +3,19 @@ class Queue {
   constructor(){
     this.elements = []
   }
-  
-  // Implementar aqui
+  size(){
+    return this.elements.length
+  }
+  add(item){
+    return this.elements.push(item)
+  }
+  peek(){
+    return this.elements[0]
+  }
+  dequeue(){
+    let tamanho = this.elements.length
+    return this.elements = this.elements.slice(1,tamanho)
+  }
 }
 
 module.exports = Queue
--- queue.test.js
+++ queue.test.js
@@ -8,18 +8,29 @@ describe('Queue', () => {
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    expect(queue.size()).toEqual(0)
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue()
+    queue.add(1)
+    expect(queue.elements[0]).toEqual(1)
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add(1)
+    queue.add(2)
+    queue.add(3)
+    expect(queue.peek()).toEqual(1)
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add(1)
+    queue.add(2)
+    queue.dequeue()
+    expect(queue.elements[0]).toEqual(2)
   })
 })
